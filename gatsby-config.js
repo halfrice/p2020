01dbb7b7552d473be3eb45dcc6ba615e49ad66d2
halfrice <halfrice@pm.me>
2020-03-22 00:16:40 -0700

    Add config and populate site manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,27 @@
-module.exports = { plugins: [`gatsby-plugin-styled-components`] }
+const config = require("./src/config/index")
+const { site, manifest } = config
+
+module.exports = {
+  siteMetadata: {
+    title: site.title,
+    description: site.description,
+    siteUrl: site.url,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: manifest.name,
+        short_name: manifest.shortName,
+        start_url: manifest.startUrl,
+        background_color: manifest.backgroundColor,
+        theme_color: manifest.themeColor,
+        display: manifest.display,
+        lang: manifest.lang,
+        icon: "./src/images/icons/logo.png",
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
+  ],
+}
---
+++ src/config/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  site: {
+    title: "P2020 - Software Engineer Website 🧐",
+    shortTitle: "P2020",
+    description: "Software Engineer Website 🧐",
+    keywords: "software, engineer, developer, code, javascript, python",
+    url: "https://github.com/halfrice/p2020",
+    prefix: "/",
+    language: "en_US",
+  },
+  manifest: {
+    name: "P2020",
+    shortName: "P2020",
+    startUrl: "/",
+    backgroundColor: "#101114",
+    themeColor: "#202124",
+    display: "minimal-ui",
+    lang: "en",
+  },
+}
